2fcd81a297145fbd74e7d6d41efef35ce665601e
Lkmrnsng <115909795+Lkmrnsng@users.noreply.github.com>
2022-10-16 11:51:21 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  char ZachFeeling[20];
+
+  printf("Hello Andrew\n");
+  printf("How are you doing Zach?");
+  scanf("%s", ZachFeeling);
+
+  printf("Oh you are %s, goodbye!", ZachFeeling);
+
+return 0;
+
+}
